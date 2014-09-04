1f793a6eff0d473e39933a2bc488e02db713b9a2
Daniel Kamhi <danielkamhi@gmail.com>
2014-09-04 11:34:53 -0400

    added destinations

--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -8,14 +8,28 @@ class DestinationsController < ApplicationController
   end
 
   def new
-  end
+    @destination = Destination.new
+  end  
 
-  def edit
-  end
+    def create
+    @destination = Destination.new(dest_params)
+    if @destination.save
+      redirect_to users_path
+    else
+      flash[:alert] = "There was a problem. Please try again."
+      render :new
+    end   
+  end 
+   def submit
+    @dest = Destination.find(params[:id])
 
-  def submit
-  	@dest = Destination.find(params[:id])
+
+  end 
+private
+
+    def dest_params
+      params.require(:destination).permit(:name, :city_id, :foodie, :adventure, :performances, :culture, :family_trip)
+    end 
 
 
-  end	
 end
